eed558c11243f5243ac446620bf77c14dff253c5
Patrick Latimer <patrick.latimer@alleninstitute.org>
2024-06-04 18:20:01 -0700

    feat: add mouse model and fetch method

---
+++ src/aind_slims_api/mouse.py
@@ -0,0 +1,70 @@
+import logging
+from typing import Annotated, Optional
+
+from slims.slims import Record
+
+from pydantic import Field, ValidationError
+
+from .core import SlimsBaseModel, SlimsClient, UnitSpec, SLIMSTABLES
+
+logger = logging.getLogger()
+
+
+class SlimsMouseContent(SlimsBaseModel):
+    baseline_weight_g: Annotated[float | None, UnitSpec("g")] = Field(
+        ..., alias="cntn_cf_baselineWeight"
+    )
+    point_of_contact: str | None = Field(
+        ..., alias="cntn_cf_scientificPointOfContact"
+    )
+    water_restricted: bool = Field(..., alias="cntn_cf_waterRestricted")
+    barcode: str = Field(..., alias="cntn_barCode")
+    pk: int = Field(..., alias="cntn_pk")
+
+    _slims_table: SLIMSTABLES = "Content"
+
+    # TODO: Include other helpful fields (genotype, gender...)
+
+    # pk: callable
+    # cntn_fk_category: SlimsColumn
+    # cntn_fk_contentType: SlimsColumn
+    # cntn_barCode: SlimsColumn
+    # cntn_id: SlimsColumn
+    # cntn_cf_contactPerson: SlimsColumn
+    # cntn_status: SlimsColumn
+    # cntn_fk_status: SlimsColumn
+    # cntn_fk_user: SlimsColumn
+    # cntn_cf_fk_fundingCode: SlimsColumn
+    # cntn_cf_genotype: SlimsColumn
+    # cntn_cf_labtracksId: SlimsColumn
+    # cntn_cf_parentBarcode: SlimsColumn
+
+
+def fetch_mouse_content(
+    client: SlimsClient,
+    mouse_name: str,
+) -> SlimsMouseContent:
+    """Fetches mouse information for a mouse with labtracks id {mouse_name}"""
+    mice = client.fetch(
+        "Content",
+        cntp_name="Mouse",
+        cntn_barCode=mouse_name,
+    )
+
+    if len(mice) > 0:
+        mouse_details = mice[0]
+        if len(mice) > 1:
+            logger.warning(
+                f"Warning, Multiple mice in SLIMS with barcode {mouse_name}, using pk={mouse_details.cntn_pk}"
+            )
+    else:
+        logger.warning("Warning, Mouse not in SLIMS")
+        return
+
+    try:
+        mouse = SlimsMouseContent.model_validate(mouse_details)
+    except ValidationError as e:
+        logger.error(f"SLIMS data validation failed, {repr(e)}")
+        return
+
+    return mouse
