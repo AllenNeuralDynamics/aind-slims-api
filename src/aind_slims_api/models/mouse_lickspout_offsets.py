330b30f50ad7a7140a3515f908015978a32615e1
Micah Woodard <micah.woodard@alleninstitute.org>
2025-05-28 13:31:24 -0700

    fixes mouse_id serialization

--- src/aind_slims_api/models/mouse_lickspout_offsets.py
+++ src/aind_slims_api/models/mouse_lickspout_offsets.py
@@ -27,8 +27,8 @@ class SlimsMouseLickspoutOffsets(SlimsBaseModel):
 
     mouse_id: Optional[str] = Field(
         default=None,
-        serialization_alias="rdrc_cf_fk_mouseID",
-        validation_alias="rdrc_cf_fk_mouseID",
+        serialization_alias="rdrc_cf_fk_mouseId",
+        validation_alias="rdrc_cf_fk_mouseId",
     )
     barcode: Optional[str] = Field(
         default=None,
